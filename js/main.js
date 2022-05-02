ce08166bc365192b01d0e7f049254cfdc395bed8
Giaki22 <giaki22@hotmail.com>
2022-05-02 18:00:14 +0200

    + Milestone 2 + 3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+const containerFizzBuzz = document.querySelector(".container");
+
 for (let i = 1; i < 101; i++) {
+    const box = document.createElement("div");
+    containerFizzBuzz.appendChild(box);
+    box.classList.add("num");
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log(`FizzBuzz ${i}`);
+        box.classList.add("fizzbuzz");
+        box.innerHTML = "FizzBuzz";
     }
     else if (i % 3 == 0) {
         console.log(`Fizz ${i}`);
+        box.classList.add("fizz");
+        box.innerHTML = "fizz";
     }
     else if (i % 5 == 0) {
         console.log(`Buzz ${i}`);
+        box.classList.add("buzz");
+        box.innerHTML = "buzz";
     }
     else {
         console.log(i);
+        box.innerHTML = i;
     }
-  }
+};
